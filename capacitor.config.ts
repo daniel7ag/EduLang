e3a2d007b81e45c32eb2df9669f9b957dde8d673
Daniel Gimenez <d.alves.gimenez7@gmail.com>
2024-11-27 22:13:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EduLang',
+  webDir: 'www'
+};
+
+export default config;
